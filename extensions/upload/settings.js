e36c3e4fe6c71cc068b1dbef46e0a1cc82bc2653
Cole <coletaylor017@gmail.com>
2020-05-01 00:06:07 -0600

    Add setting for AWS direct upload

---
+++ extensions/upload/settings.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "provider": "aws-s3",
+    "providerOptions": {
+        "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
+        "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
+        "region": process.env.AWS_REGION,
+        "params": {
+            "Bucket": "rellios-direct-uploads"
+        }
+    }
+}
